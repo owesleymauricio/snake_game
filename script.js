63e44a4fb8c31f3c12825f32f1d91fbe882cdd2b
owesleymauricio <wesleymauricio85@gmail.com>
2024-05-03 19:40:21 -0300

    update

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ const contexto = canvas.getContext("2d")
 //contexto.fillRect(250, 250, 50, 50) // define a altitude e logngitude, altura e latgura
 
 const size = 20 // tamanho da comida e da cobrinha
-const snake = [
-    {x:200, y:200},
-    {x:220, y:200}
-] // array que fazz a cobra se mover, cada pade da cobra e um objeto
+const snake = [{x:0, y:0}] // array que fazz a cobra se mover, cada pade da cobra e um objeto
 
 
-let direction = "right"
+let direction
+
+let loopID // limpa o browser para na duplicar os loops
 
 const drawnSnake = () => {
     contexto.fillStyle = "#ddd"
@@ -19,30 +18,68 @@ const drawnSnake = () => {
     snake.forEach((position, index) =>{
 
         if(index === snake.length - 1){
-            contexto.fillStyle = "blue"
+            contexto.fillStyle = "aliceblue"
         }
         contexto.fillRect(position.x, position.y, size, size)
     })
 }
 
+// controla e move a direção da snake 
 const moveSnake = () => {
     if(!direction) return // se noa tiver nada pula direto pro final da função 
 
     const head = snake.at(-1) // a cabeça da cobrinha
 
     
-
+    
     if(direction == "right"){
         snake.push({x:head.x + size, y: head.y})
+    }
+    
+    if(direction == "left"){
+        snake.push({x:head.x - size, y: head.y})
+    }
+    
+    if(direction == "down"){
+        snake.push({x:head.x , y: head.y + size})
+    }
+    
+    if(direction == "up"){
+        snake.push({x:head.x , y: head.y - size})
     }
     snake.shift() // pega o ultimo objeto do array
 }
 
-setInterval(() => {
-    contexto.clearRect(0,0,500,500)
+//vai fazer o jogo rodar
+const gameLoop = () => {
+    clearInterval(loopID)
+    contexto.clearRect(0,0,500,500)//limpa a tela
 
-    drawnSnake()
-    moveSnake()
-}, 300)
-   
+    drawnSnake() // create snake
+    moveSnake() // move a cobrinha
+
+    //chama a função para ficar sempre rodando
+    loopID = setTimeout(() => {
+        gameLoop()
+    }, 300)
+}
+
+gameLoop()
+
+//teclas para mover a snake
+document.addEventListener("keydown", ({key}) =>{
+    if(key === "ArrowRight" && direction != 'left'){
+        direction = 'right'
+    }
+    if(key === "ArrowLeft" && direction != 'right'){
+        direction = 'left'
+    }
+    if(key === "ArrowDown" && direction != 'up'){
+        direction = 'down'
+    }
+    if(key === "ArrowUp" && direction != 'down'){
+        direction = 'up'
+    }
+})
+
 
